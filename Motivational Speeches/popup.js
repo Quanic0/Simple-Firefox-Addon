503b8dcd5a6e93c3b98c924bc7340b7399250a14
Quanic0 <163568245+Quanic0@users.noreply.github.com>
2024-03-18 22:42:27 +0300

    Add files via upload

--- Motivational Speeches/popup.js
+++ Motivational Speeches/popup.js
@@ -71,10 +71,11 @@ function getRandomNumber() {
   document.addEventListener('DOMContentLoaded', function() {
     const initialSpeech = getRandomSpeech();
     displaySpeech(initialSpeech); // Display initial speech when popup is opened initially
-      setInterval(getAndDisplaySpeech, 120000);
+    
     document.getElementById('getSpeech').addEventListener('click', function() {
       const newSpeech = getRandomSpeech();
       displaySpeech(newSpeech);
+      setInterval(getAndDisplaySpeech, 120000);
     });
   });
   
